1cc32780a00b14467388d148e8d4f7000da5ee03
Roc <597540267@qq.com>
2020-11-10 17:59:04 +0800

    Update rc522.c

--- rc522.c
+++ rc522.c
@@ -12,8 +12,8 @@
 #include "stm32f10x.h"
 
 void SPI1_Init() {
- 	GPIO_InitTypeDef GPIO_InitStructure;
-  SPI_InitTypeDef  SPI_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	SPI_InitTypeDef  SPI_InitStructure;
 
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
