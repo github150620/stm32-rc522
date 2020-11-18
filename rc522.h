33f962b98ebe3a9ef571cd5342f6319b8bb350fb
Roc <597540267@qq.com>
2020-11-18 10:59:09 +0800

    Update rc522.h

--- rc522.h
+++ rc522.h
@@ -7,6 +7,7 @@ void RC522_Config (void);
 
 u8 RC522_Request  (u16 *type);
 u8 RC522_Anticoll (u8 *id);
+u8 RC522_Select   (u8 *id);
 u8 RC522_Auth     (u8 mode, u8 addr, u8 *key, u8 *id);
 u8 RC522_Read     (u8 addr, u8 *values);
 u8 RC522_Write    (u8 addr, u8 *values);
